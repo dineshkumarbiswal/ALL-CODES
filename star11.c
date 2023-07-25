1831ca727c56e00d2d5acd8cf4707a64a18a3fb9
DINESH KUMAR BISWAL <dineshkumarbiswal@DINESHs-MacBook-Air.local>
2023-07-25 22:23:39 +0530

    add all file

---
+++ star11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k = 0, rows;
+    printf("enter number of row");
+    scanf("%d", &rows);
+    for (i = 1; i <= rows; i++)
+    {
+        if (rows % 2 == 0)
+        {
+            if (i <= rows / 2)
+                k++;
+            if (i > rows / 2 + 1)
+                k--;
+        }
+        else
+            i <= (rows + 1) / 2 ? k++ : k--;
+        for (j = 1; j <= (rows + 1) / 2; j++)
+        {
+            if (j <= k)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ star14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, k;
+    for (i = 1; i <= 7; i++)
+    {
+        k = 7 - i;
+        for (j = 1; j <= 7; j++)
+        {
+            if (j >= 1 && j <= 8 - i){
+                printf("%d", k);
+                k--;
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
