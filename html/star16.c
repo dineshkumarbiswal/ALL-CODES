eb9bb66831a21ac07c8c1f4aaf41ec3d853d4b87
dinesh kumar biswal <dineshkumarbiswal999@gmail.com>
2023-07-26 10:35:19 +0530

    second commit

---
+++ html/star16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 7; i++)
+    {
+        for (j = 1; j <= 7; j++)
+        {
+            if (j == i || j == 8 - i){
+                if(i==8-j)
+                printf("/");
+                else
+                    printf("\\");
+            }
+            else
+                printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ html/star19.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    char k;
+    for (i = 1; i <= 4; i++)
+    {
+        k='A';
+        for (j = 1; j <= 7; j++)
+        {
+            if (j>=5-i&&j<=3+i){
+                printf("%c",k);
+                i<=4?k++:k--;
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
